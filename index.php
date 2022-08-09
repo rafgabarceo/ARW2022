213c90dd42f04f3d4a4f0ee6051034e8b8a03ee6
kfcampit <kyle_campit@dlsu.edu.ph>
2022-08-09 17:21:27 +0800

    removed countdown

--- index.php
+++ index.php
@@ -3,43 +3,42 @@
 ?>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="wWx`idth=device-width, initial-scale=1.0">
     <title>ARWonderland</title>
-    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Berkshire Swash">
+    <?php require_once('head.php')?>
     <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/rabbit_hole.css">
-
-
-    <style>body{font-family: "Berkshire Swash";};</style>
-
+    <link rel="stylesheet" href="./css/clouds.css">
 </head>
-
 <body>
-    <div class="moving_bg" id="bg-div" style = "background-image: url('./assets/arw_rabbit_hole/RABBIT HOLE/BG RABBIT HOLE.png')">
-        <img src="./assets/arw_rabbit_hole/RABBIT HOLE/CORE.png" id="_fixed_layers" class="_bg_moving">
-        <img src="./assets/arw_rabbit_hole/RABBIT HOLE/1STLAYER_MIDDLE.png" id="_moving_layer1" class="_bg_moving">
-        <img src="./assets/arw_rabbit_hole/RABBIT HOLE/2NDLAYER_MIDDLE.png" id="_moving_layer2" class="_bg_moving">
-        <img src="./assets/arw_rabbit_hole/RABBIT HOLE/3RDLAYER_MIDDLE.png" id="_moving_layer3" class="_bg_moving">
-        <img src="./assets/arw_rabbit_hole/RABBIT HOLE/3RDLAYER_MIDDLE.png" id="_moving_layer4" class="_bg_moving">
-        <img src="./assets/arw_rabbit_hole/RABBIT HOLE/Rabbit Hole Core.png" id="_core" class="_bg_moving">
-    </div>
     
-    <div class="container centered rounded bg-dark opacity-75 text-center text-white">
-        <!-- TITLE -->
-        <h1 class = "countdown-font">ARW 2022</h1>
-        <!-- TIMER -->
-        <script language="JavaScript">
-            TargetDate = "2022-09-23T05:00:00";
-            CountActive = true;
-            CountStepper = -1;
-            LeadingZero = true;
-            FinishMessage = "It is finally here!";
-        </script>
-        <!-- Timer javascript -->
-        <script language="JavaScript" src="./js/countdown.js"></script>
+
+    <?php require_once('nav_bar.php') ?>
+        
+    <!-- First DiV -->
+    <div class="center-cropped" style = "background-image: url('./assets/arw_cover_bg/cover_bg_noclouds.png'); height: 100vh; z-index: 1">
+            <!-- Desktop ver -->
+            <div class="container d-none d-lg-block" style="position: relative; top: -18%;">
+                <img src="./assets/arw_cover_bg/cover_text.png" style="width:100%; z-index: 3;" alt="">
+                
+            </div>
+            <img src="./assets/arw_cover_bg/moving_clouds.png" class="clouds_moving d-none d-lg-block" id="_clouds" alt="">
+
+            <!-- Mobile ver -->
+            <div class="container d-lg-none" style="position: relative;">
+                <img src="./assets/arw_cover_bg/cover_text.png" class="img-fluid" alt="">
+            </div>
+
+            <img src="./assets/arw_cover_bg/moving_clouds.png" class="clouds_mobile d-lg-none" id="_clouds" alt="">
+
+            <!-- Both -->
+            
+
+
+        </div>
+        
+    </div>
+    <div class="center-cropped" style = "background-image: url('./assets/arw_cover_bg/cover_bg.png'); height: 100vh;">
+        <!-- <img src="./assets/arw_cover_bg/COVER_BG.png" class="img-fluid" alt=""> -->
+        testing
     </div>
 
 
