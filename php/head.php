29b0e2b8ea4eb8721ac22baaf41d42f36c5127a8
Rafael Gabriel Arceo <arceo.rafaelgabriel@protonmail.com>
2022-08-13 19:02:55 +0800

    Recommit to fix viewport typo

--- php/head.php
+++ php/head.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="wWx`idth=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
