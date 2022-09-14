2d22857509cc424ec6648ea00a39184cc6056177
kyoseia <87558896+kyoseia@users.noreply.github.com>
2022-09-14 22:37:13 +0800

    Added the entire design as placeholder, without Nav Shorcuts, Buttons, and Assets

--- php/arw_elite.php
+++ php/arw_elite.php
@@ -22,54 +22,196 @@
     .custom-bg {
         background-color: rgb(255, 255, 255, 0.85);
     }
+    .placeholder-bg1 {
+        background-color: rgb(34, 118, 0.50);
+    }
     .custom-bg-green{
-        background-color: rgb(34, 118, 0.50)
+        background-color: rgb(34, 118, 0.50, 0.50)
     }
     .custom-bg-orange {
         background-color: rgb(244, 162, 56, 0.85);
     }
 
-    body {
-  background: url('../assets/arw_cover_bg/cover_bg_noclouds.png') no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  background-size: cover;
-  -o-background-size: cover;
-}
     </style>
 
     <?php require_once('nav_bar_2.php') ?>
+    <div class="left-cropped" style="background: white; height: 70px; z-index: -1"></div>
 
-    <div class="center-cropped" style="background: white; height: 75px; z-index: -1"></div>
+    <!-- First Sec -->
 
-  <div class="px-4 py-5 my-5 text-center">
-    <h1 class="display-5 fw-bold">The ARW Elite</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">ARW Elite aims to highlight the goals, culture, and values of the 49 organizations under the Council of Student Organizations through a series of online challenges. The Ultimate ARW Elite, best embodies the character of their organization and is a great example which the Lasallian community would look up to.</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      Catch your Org’s Elite on the ARW Elite Coronation Night this "insert date". You may show support for your bets in our Facebook livestream by clicking the Facebook Icon. See you there!
-      <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Facebook</button>
-      </div>
-    </div>
-  </div>
-
-  <div class="container custom-bg my-5">
-    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border-dark shadow-lg">
-      <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-        <h1 class="display-4 fw-bold lh-1">The ARW Elite</h1>
-        <p class="lead">ARW Elite aims to highlight the goals, culture, and values of the 49 organizations under the Council of Student Organizations through a series of online challenges. The Ultimate ARW Elite, best embodies the character of their organization and is a great example which the Lasallian community would look up to.</p>
-        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+    <section class="text-light p-5 px-1 pt-md-5 text-center" style="background: pink">
+    <div class="container custom-bg-green p-sm-0 my-2 rounded-4">
+        <div class="p-3">
+                <h1 class="pt-4">The ARW Elite</h1>
+                <p class="lead px-5 text-justify" style="font-family:Garamond">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada ipsum sit amet leo interdum, vitae maximus turpis pharetra. 
+                Etiam lobortis nibh sed gravida condimentum. Maecenas luctus congue commodo. Aliquam volutpat nisi ac purus semper, vitae auctor elit hendrerit. 
+                Donec ac mauris a nunc tempor mollis. Praesent finibus, ligula vitae tincidunt dignissim, risus ante dictum sapien, et condimentum nibh nulla vitae nunc. 
+                Etiam quis placerat tortor. Donec venenatis, ipsum at vehicula molestie, risus dolor pellentesque mi, eu pharetra magna neque ac metus. 
+                Duis mollis neque ex, ut bibendum justo tempus sit amet. Suspendisse ut vulputate quam. Cras accumsan ornare congue. Sed sagittis fringilla felis quis venenatis. 
+                Morbi rhoncus at ligula ac ultricies. Vestibulum lobortis iaculis nisl. Vivamus nec augue venenatis, tristique dolor id, accumsan massa.
+                </p>
+            <button class="btn btn-lg text-light" style="background: #3b5998">Facebook</button>
         </div>
-      </div>
-      <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
-          <img class="rounded-lg-3" src="..\assets\org_images\access-logo.webp" alt="ACCESS LOGO" width="720">
-      </div>
     </div>
-  </div>
+    </section>
 
+    <!-- Second Sec -->
 
+    <section class="text-light p-5 text-center" style="background: #5e51a2">
+    <div><h1 class="p-3">The ARW Elite Finalists</h1>
+    <div class="container">
 
+            <div class="row justify-content-center">
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-4">
+                    <div class="row pt-4"><div class="container bg-info rounded-5" style="height: 400px; width: 200px"></div></div>
+                        <div class="row py-4"><div class="container bg-info rounded-4" style="height: 75px; width: 200px">
+                            <h5 class="pt-2" style="font-family:Garamond">ELITE NAME</p>
+                            <h5 class="pb-5" style="font-family:Garamond; font-weight:bold">ORG NAME</p>
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+    </section>
+
+    <!-- Third Sec -->
+    <section class="text-dark p-5 text-center" style="background: pink">
+        <div><h1 class="p-3">GET TO KNOW THE ELITE FINALISTS</h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="row pt-4 justify-content-md-center">
+                            <div class="container m-3 bg-light rounded-5" style="height: 400px; width: 200px"></div>
+                            <div class="container m-3 bg-light rounded-5" style="height: 400px; width: 200px"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 text-start">
+                        <h1 class="pt-4" style="font-family:Garamond">ORG FULL NAME</h1>
+                        <h3 class="" style="font-family:Garamond">ORG NAME</h3>
+                        <h2 class="" style="font-family:Garamond">ELITE NAME</h2>
+                        <div class="container bg-light" style="height: 360px; width: 640px"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Fourth Sec -->
+    <section class="text-light p-5 text-center" style="background: #5e51a2">
+        <div>
+            <div class="container">
+                <div class="row">
+                    <div class="col text-end">
+                        <div class="container">
+                            <h1 class="pt-4" style="font-family:Garamond">ORG FULL NAME</h1>
+                            <h3 class="" style="font-family:Garamond">ORG NAME</h3>
+                            <h2 class="" style="font-family:Garamond">ELITE NAME</h2>
+                            <div class="container bg-light" style="height: 360px; width: 640px"></div>
+                        </div>
+                            
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="row pt-4 justify-content-md-center">
+                            <div class="container m-3 bg-light rounded-5" style="height: 400px; width: 200px"></div>
+                            <div class="container m-3 bg-light rounded-5" style="height: 400px; width: 200px"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- LAST SEC -->
+    <section class="text-light p-5 px-1 pt-md-5 text-center" style="background: pink">
+        <div class="justify-content-center">
+            <h1 class="p-3 text-dark">Who will be the ultimate ARW Elite?</h1>
+            <div class="container bg-light p-sm-0 my-2" style="height: 70vh; width: 1000vh">
+            </div>
+        </div>
+    </section>
 
 
 
