995cc020313164fdc37cde6cce9eb64459517339
Jeyell 26 <john_louise_bungay@dlsu.edu.ph>
2022-09-26 18:03:33 +0800

    Fixed cencomm sections, landing, who we are, pheads

--- php/comm_page.php
+++ php/comm_page.php
@@ -3,7 +3,7 @@
     <head>
     <?php require_once('head_2.php')?>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <link rel="stylesheet" type="text/css" href="../css/styletest.css">
+    <link rel="stylesheet" type="text/css" href="../css/comm.css">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
 
     </head>
@@ -24,19 +24,30 @@
  
     <!-- SECTION 2: WHO WE ARE -->
 
-     <section style = "height: 100vh;">
-        <div class="container mx-auto rounded w-75 bg-white opacity-50 text-xl-center mt-4" style = "height: 50vh;" >
-            <h1 style = "font-size:72px;">WHO WE ARE</h1>
-            <p class = "mx-5" style = "font-family: 'Garamond'; font-size:35px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <section style = "height: 100vh;">
+        <img class = "headerPic" src="../assets/arw_cencomm/WhoWeAre.png" style="width:100%; z-index: 3;" alt="">
+        <div class="container mx-auto rounded w-75 text-white text-xl-center mt-4" style = "height: 50vh; background: rgba(216, 45, 39, 0.75);" >
+            <p class = "mx-5" style = "font-family: 'EB Garamond'; font-size:46px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
     </section>
 
-    <!-- SECTION 3: PHEADS -->
     <section style = "height: 100vh;">
-        <div class="container w-100 bg-white opacity-50 text-xl mt-4" style = "width: 100%;">
-            <h1 class = "mx-3" style = "font-size:72px;">Project Heads</h1>
-            <p class = "mx-3" style = "font-family: 'Garamond'; font-size:22px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>   
+        <img class = "headerPicLeft" src="../assets/arw_cencomm/phead.png" style="z-index: 3;" alt="">
+        <div class="rounded text-white text-xl-left mt-4" style = "width: 1378px; height: 158px; margin-left: 12%; background: rgba(216, 45, 39, 0.75);" >
+            <p class = "mx-5" style = "font-family: 'EB Garamond'; font-size:33px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <span>
+            <img class = "commPic" src="../assets/arw_cencomm/cencomm.png" style="z-index: 3;" alt="">
+        </span>
+        <span>
+            <img class = "commPic" src="../assets/arw_cencomm/cencomm.png" style="z-index: 3; margin-left: -20%;" alt="">
+        </span>
+        <span>
+            <img class = "commPic" src="../assets/arw_cencomm/cencomm.png" style="z-index: 3; margin-left: -20%;" alt="">
+        </span>
+        <span>
+            <img class = "commPic" src="../assets/arw_cencomm/cencomm.png" style="z-index: 3; margin-left: -20%;" alt="">
+        </span>
     </section>
 
     </body>
