3ba42b7bef7861b2807fc4c4bf6439b3acec96c6
kyoseia <87558896+kyoseia@users.noreply.github.com>
2022-09-28 00:35:57 +0800

    Re-committing Placeholder design of Mobile version of ARW Elite

--- php/arw_elite.php
+++ php/arw_elite.php
@@ -43,6 +43,10 @@
         transform: translate(-50%, -50%);
     }
 
+    .text-class{
+        text-align: justify;
+    }
+
     </style>
 
     <?php require_once('nav_bar_2.php') ?>
@@ -52,8 +56,8 @@
 
     <!-- First Sec -->
 
-    <section class="text-light p-5 px-1 pt-md-5 text-center" style="background: pink; height:100vh">
-    <div class="container custom-bg-black p-sm-0 my-2 rounded-4 centerabs">
+    <section class="text-light p-5 px-1 pt-md-5 text-center d-none d-sm-none d-md-none d-lg-block d-xl-block" style="background: pink; height:100vh">
+    <div class="container custom-bg-black p-sm-0 my-2 rounded-4 centerabs text-center">
         <div class="p-3">
                 <h1 class="pt-4">The ARW Elite</h1>
                 <p class="text-class lead px-5" style="font-family:Garamond">
@@ -71,7 +75,7 @@
 
     <!-- Second Sec -->
 
-    <section class="text-light p-5 text-center" style="background: #5e51a2">
+    <section class="text-light p-5 text-center d-none d-sm-none d-md-none d-lg-block d-xl-block" style="background: #5e51a2">
     <div><h1 class="pt-3 pb-1">The ARW Elite Finalists</h1>
     <div class="container px-5" style="width: auto">
 
@@ -172,7 +176,7 @@
     </section>
 
     <!-- Third Sec -->
-    <section class="text-dark p-5 pb-4 text-center" style="background: pink; height:100vh">
+    <section class="text-dark p-5 pb-4 text-center d-none d-sm-none d-md-none d-lg-block d-xl-block" style="background: pink; height:100vh">
         <div><h1 class="p-5">GET TO KNOW THE ELITE FINALISTS</h1>
             <div class="container">
                 <div class="row">
@@ -194,7 +198,7 @@
     </section>
 
     <!-- Fourth Sec -->
-    <section class="text-light p-5 text-center" style="background: #5e51a2; height: 100vh">
+    <section class="text-light p-5 text-center d-none d-sm-none d-md-none d-lg-block d-xl-block" style="background: #5e51a2; height: 100vh">
         <div>
             <div class="container" style="position:relative;">
                 <div class="row">
@@ -219,7 +223,7 @@
     </section>
 
     <!-- LAST SEC -->
-    <section class="text-light p-5 px-1 pt-md-5 text-center" style="background: pink; height:95vh">
+    <section class="text-light p-5 px-1 pt-md-5 text-center d-none d-sm-none d-md-none d-lg-block d-xl-block" style="background: pink; height:95vh">
         <div class="justify-content-center">
             <h1 class="p-5 pb-4 text-dark">Who will be the ultimate ARW Elite?</h1>
             <div class="container bg-light p-sm-0 my-2" style="height: 70vh; width: 1000vh">
@@ -229,10 +233,171 @@
 
 
 
-            <!-- Desktop ver -->
+    <!-- Desktop ver -->
+
+    <!-- First Sec -->
+
+    <section class="text-light p-5 px-1 pt-md-5 text-center d-block d-sm-block d-md-block d-lg-none d-xl-none" style="background: pink; height:100vh">
+    <div class="container custom-bg-black p-sm-0 my-2 rounded-4 text-center">
+        <div class="p-3">
+                <h1 class="pt-4">The ARW Elite</h1>
+                <p class="text-class lead px-3 px-md-4" style="font-family:Garamond">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque malesuada ipsum sit amet leo interdum, vitae maximus turpis pharetra. 
+                Etiam lobortis nibh sed gravida condimentum. Maecenas luctus congue commodo. Aliquam volutpat nisi ac purus semper, vitae auctor elit hendrerit. 
+                Donec ac mauris a nunc tempor mollis. Praesent finibus, ligula vitae tincidunt dignissim, risus ante dictum sapien, et condimentum nibh nulla vitae nunc. 
+                Etiam quis placerat tortor. Donec venenatis, ipsum at vehicula molestie, risus dolor pellentesque mi, eu pharetra magna neque ac metus. 
+                Duis mollis neque ex, ut bibendum justo tempus sit amet. Suspendisse ut vulputate quam. Cras accumsan ornare congue. Sed sagittis fringilla felis quis venenatis. 
+                Morbi rhoncus at ligula ac ultricies. Vestibulum lobortis iaculis nisl. Vivamus nec augue venenatis, tristique dolor id, accumsan massa.
+                </p>
+            <button class="btn btn-lg text-light" style="background: #3b5998">Facebook</button>
+        </div>
+    </div>
+    </section>        
+
+    <!-- Second Sec -->
+
+    <section class="text-light p-md-5 p-sm-5 p-xs-3 text-center d-block d-sm-block d-md-block d-lg-none d-xl-none" style="background: #5e51a2">
+    <div><h1 class="pt-3 pb-1">The ARW Elite Finalists</h1>
+    <div class="container px-3 p-sm-5" style="width: auto">
+
+            <!-- First Row -->
+            <div class="row justify-content-center">
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col px-2">
+                    <div class="row pt-1"><div class="container bg-info rounded-4" style="height: 300px; width: 19vh"></div></div>
+                        <div class="row pt-1 pb-4 px-3"><div class="container bg-info rounded-4" style="height: 45px; width: 19vh">
+                            <h6 class="pt-1" style="font-family:Garamond">Sample Name</h6>
+                            <h5 class="pb-0" style="position: relative; top: -10px;font-family:Garamond; font-weight:bold">ORG NAME</h5>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+    </section>
+
+     <!-- Third Sec -->
+
+     <section class="text-dark text-center d-block d-sm-block d-md-block d-lg-none d-xl-none" style="background: pink; height:100vh">
+        <div><h1 class="p-5">GET TO KNOW THE ELITE FINALISTS</h1>
+            <div class="container px-3">
+            <div class="row justify-content-center">
+                <div class="col px-2"><div class="row pt-1"><div class="container bg-light rounded-4" style="height: 300px; width: 19vh"></div></div></div>
+                <div class="col px-2"><div class="row pt-1"><div class="container bg-light rounded-4" style="height: 300px; width: 19vh"></div></div></div>
+            </div>
+
+            <div class="col text-center">
+                <h1 class="pt-3" style="font-family:Garamond">ORG FULL NAME</h1>
+                <h3 class="" style="font-family:Garamond;position: relative; top: -15px">ORG NAME</h3>
+                <h2 class="" style="font-family:Garamond;position: relative; top: -30px">ELITE NAME</h2>
+                <div class="container bg-light" style="height: 30vh; width: 75vw"></div>
+            </div>
+            </div>
+        </div>
+    </section>
+
+     <!-- Fourth Sec -->
+    
+     <section class="text-light text-center d-block d-sm-block d-md-block d-lg-none d-xl-none" style="background: #5e51a2; height:100vh">
+        <div><h1 class="p-5">GET TO KNOW THE ELITE FINALISTS</h1>
+            <div class="container px-3">
+            <div class="row justify-content-center">
+                <div class="col px-2"><div class="row pt-1"><div class="container bg-light rounded-4" style="height: 300px; width: 19vh"></div></div></div>
+                <div class="col px-2"><div class="row pt-1"><div class="container bg-light rounded-4" style="height: 300px; width: 19vh"></div></div></div>
+            </div>
+
+            <div class="col text-center">
+                <h1 class="pt-3" style="font-family:Garamond">ORG FULL NAME</h1>
+                <h3 class="" style="font-family:Garamond;position: relative; top: -15px">ORG NAME</h3>
+                <h2 class="" style="font-family:Garamond;position: relative; top: -30px">ELITE NAME</h2>
+                <div class="container bg-light" style="height: 30vh; width: 75vw"></div>
+            </div>
+            </div>
+        </div>
+    </section>
+
 
     <!-- LAST SEC -->
-    <section class="text-light p-5 px-1 pt-md-5 text-center d-block d-sm-block d-md-block d-lg-none d-xl-none" style="background: pink">
+    <section class="text-light p-5 px-1 text-center d-block d-sm-block d-md-block d-lg-none d-xl-none" style="background: pink">
         <div class="justify-content-center">
             <h1 class="p-3 text-dark">Who will be the ultimate ARW Elite?</h1>
             <div class="container bg-light p-sm-0 my-2" style="height: 50vw; width: 75vw">
