efe8092bc6e4b2a34f466df547c73fa85c997007
Ethea2 <nathandres2001@gmail.com>
2022-10-01 19:51:08 +0800

    added final changes.

--- php/organizations.php
+++ php/organizations.php
@@ -46,8 +46,8 @@
                 </div>
                 <div class="cso-title-div">
                     <p class="cso-text">Council of Student Organizations</p>
-                    <p class="cso-sub-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
+                <button class="btn btn-success rounded-pill align-self-center" style="width: 50%; height:3rem;">Button</button>
             </div>
         </div>
         <!-- Mobile Version -->
@@ -57,8 +57,8 @@
                 </div>
                 <div class="cso-title-div">
                     <p style="font-size: 36px; text-align: center;">Council of Student Organizations</p>
-                    <p style="font-size: 16px; text-align: center;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
+                <button class="btn btn-success rounded-pill align-self-center" style="width: 50%; height:3rem;">Button</button>
             </div>
         </div>
     </section>
