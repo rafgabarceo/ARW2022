976f8e9fdeb125f711eca4b2748f702278337d2e
Luke Cuello <raphael_cuello@dlsu.edu.ph>
2022-10-02 17:55:38 +0800

    Added Diamond Sponsors

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <?php require_once('./php/head.php')?>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/clouds.css">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 </head>
 <body>
     
@@ -18,7 +19,7 @@
     <!-- First Div -->
     <div class="center-cropped" style = "background-image: url('./assets/arw_cover_bg/cover_bg_noclouds.png'); background-attachment: fixed; height: 100vh; z-index: 1">
             <!-- Desktop ver -->
-            <div class="container d-none d-lg-block" style="position: relative; top: -18%;">
+            <div class="container d-none d-lg-block" style="position: relative; top: -50px;">
                 <img src="./assets/arw_cover_bg/cover_text.png" style="width:100%; z-index: 3;" alt="">
                 
             </div>
@@ -116,20 +117,98 @@
         </div> 
     </section>
     <!-- Third Div -->
-    <section class="center-cropped" style = "background-color: #FFFFFF; height: calc(100vh - 70px);  overflow: hidden;">
+    <section class="center-cropped" style = "background-color: #FFFFFF; height: 150vh;  overflow: hidden;">
+        <!-- Desktop version -->
         <div class="container mx-auto rounded py-5 px-1 w-75 text-xl-center mt-4"></div>
             <h1 class="m-3" style = "font-family: 'EB Garamond'; text-align:center;">
                 We would like to thank the following
             </h1>
         </div>
-        <div class="container mx-auto swiper">
-
+        <div class="container d-none d-lg-block mx-auto swiper">
+            <!-- Diamond Sponsors -->
+            <div class="row">
+                <div class="container">
+                    <h2 class = "p-4" style = "font-family: 'EB Garamond'; text-align:center;">Diamond Sponsors</h2>
+                    <section class="logo-carousel slider" data-arrows="true">
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/_Ask Lex PH Academy.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/01 - Bukas.png" style="height: auto; width: 90%; margin-right: -800px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/1_logowhite.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/02 - SSV .png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/03 -  HENRY + LUCY.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/04 - Rae Lifestyle .png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/05 - UFOTO.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/06 - Mazars .png" style="height: auto; width: 100%; margin-right: -1250px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/07 - Accenture .png" style="height: auto; width: 100%; margin-right: -1250px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/08 - Amihan.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/09 - Bratpack.png" style="height: auto; width: 90%; margin-right: -800px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/B_lue Logo.jpg" style="height: auto; width: 100%; margin-right: -3050px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/Great Taste Logo.png" style="width: 120%; margin-right: -160px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/Japanmatcha.ph logo.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/MINILA_LOGO NEW_RL TRANSPARENT.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/MSI 3.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/SACO-Logo [WHITE].png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/Shopee_logo_white.png"></div>
+                    </section>
+                </div>
+            </div>
+            <!-- Platinum Sponsors -->
+            <div class="row">
+                <div class="container">
+                    <h2 class = "p-4" style = "font-family: 'EB Garamond'; text-align:center;">Platinum Sponsors</h2>
+                    <section class="logo-carousel slider" data-arrows="true">
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/_Ask Lex PH Academy.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/01 - Bukas.png" style="height: auto; width: 90%; margin-right: -800px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/1_logowhite.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/02 - SSV .png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/03 -  HENRY + LUCY.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/04 - Rae Lifestyle .png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/05 - UFOTO.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/06 - Mazars .png" style="height: auto; width: 100%; margin-right: -1250px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/07 - Accenture .png" style="height: auto; width: 100%; margin-right: -1250px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/08 - Amihan.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/09 - Bratpack.png" style="height: auto; width: 90%; margin-right: -800px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/B_lue Logo.jpg" style="height: auto; width: 100%; margin-right: -3050px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/Great Taste Logo.png" style="width: 120%; margin-right: -160px;"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/Japanmatcha.ph logo.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/MINILA_LOGO NEW_RL TRANSPARENT.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/MSI 3.png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/SACO-Logo [WHITE].png"></div>
+                        <div class="slide"><img class = "slide-img"  src="./assets/sponsors/Logos/[1] Diamond Logos/Shopee_logo_white.png"></div>
+                    </section>
+                </div>
+            </div>
         </div>
-
     </section>
     <!-- Footer -->
     
     
     <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <script type = "text/javascript">
+       $(document).ready(function(){
+        $('.logo-carousel').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            variableWidth: true,
+            centerMode: true,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            arrows: false,
+            dots: false,
+            pauseOnHover: false,
+            responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4
+                }
+            }, {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 3
+                }
+            }]
+        });
+    });
+    </script>
 </body>
 </html>
