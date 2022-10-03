e9ac961a8c951dbb7af182e69bce30952d996c80
Luke Cuello <raphael_cuello@dlsu.edu.ph>
2022-10-04 03:00:58 +0800

    Fixed floater again

--- php/floater.php
+++ php/floater.php
@@ -1,17 +1,11 @@
-<div class="soc-med-container ml-3" style = "position: fixed; top: 50%; transform: translateY(-50%); z-index: 10; ">
-    <div class="soc-med-btn">
-        <a href="https://www.facebook.com/DLSUARW" rel = "noreferrer" target="_blank" class="btn gradient-anim py-1 p-4 mb-1" style = "display: inline-block; pointer-events: auto; width: 50px; height: 50px; border-radius: 50%">
-            <i style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)" class="fab fa-facebook-f"></i>
-        </a>
-    </div>
-    <div class="soc-med-btn">
-        <a href="https://www.dlsucso.com" rel = "noreferrer" target="_blank" class="btn gradient-anim py-1 p-4 mb-1" style = "display: inline-block; pointer-events: auto; width: 50px; height: 50px; border-radius: 50%">
-            <i style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)" class="fab fa-facebook-f"></i>
-        </a>
-    </div>
-    <div class="soc-med-btn">
-        <a href="https://www.instagram.com/dlsu_arw/" rel = "noreferrer" target="_blank" class="btn gradient-anim py-1 p-4 mb-1" style = "display: inline-block; pointer-events: auto; width: 50px; height: 50px; border-radius: 50%">
-            <i style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)" class="fab fa-instagram"></i>
-        </a>
-    </div>
+<div class="soc-med-container ml-4" style = "position: fixed; top: 50%; transform: translateY(-50%); z-index: 1000; ">
+    <a href="https://www.facebook.com/DLSUARW" rel = "noreferrer" target="_blank" class="btn btn-danger btn-lg btn-floating" style = "display: block; pointer-events: auto; width: 50px; height: 50px; border-radius: 50%" role = "button">
+        <i class="fa fa-facebook"></i>
+    </a>
+    <a href="https://www.dlsucso.com" rel = "noreferrer" target="_blank" class="btn btn-danger btn-lg btn-floating gradient-anim mt-1" style = "display: block; pointer-events: auto; width: 50px; height: 50px; border-radius: 50%" role = "button">
+        <i class="fa fa-window-maximize"></i>
+    </a>
+    <a href="https://www.instagram.com/dlsu_arw/" rel = "noreferrer" target="_blank" class="btn btn-danger btn-lg btn-floating gradient-anim mt-1" style = "display: block; pointer-events: auto; width: 50px; height: 50px; border-radius: 50%" role = "button">
+        <i class="fa fa-instagram"></i>
+    </a>
 </div>
